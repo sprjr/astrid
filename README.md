7c4e156545ead53b793d1b27a5b4524f5d18ea90
srivas <srivas@netflix.com>
2014-12-19 17:13:32 -0800

    Simple proxy example

--- README.md
+++ README.md
@@ -3,3 +3,12 @@ It's simple, proxying makes things better.
 
 ## About
 A simple node proxy module that aspires to be like Zuul.
+
+
+## Alpha Example
+
+Usage:
+- fire up anything on port 3000 (e.g., an express app)
+- node astrid.js
+- curl http://localhost:3001/localhost/any/path/here
+// -> You'll get a stream from http://localhost:3000/any/path/here
--- astrid.js
+++ astrid.js
@@ -1,1 +1,25 @@
 // code might be typed in this file soon™
+
+var restify = require('restify');
+var request = require('request');
+
+var server = restify.createServer();
+
+server.get(/^\/([a-zA-Z0-9_\.~-]+)\/(.*)/, proxy);
+
+server.listen(3001, function () {
+    console.log('proxying on %d', 3001);
+});
+
+
+// Alpha stuff
+function proxy (req, res, next) {
+    var base = req.params[0];
+    var path = req.params[1];
+    var port = 3000;
+    var uri = 'http://' + base + ':' + port + '/' + path;
+
+    console.log('proxying uri: ' + uri);
+
+    request(uri).pipe(res);
+}
